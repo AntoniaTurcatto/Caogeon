9e8c37d11acf2c17339c01d7341b34411767f75c
antoniaturcatto <antoniaturcatto29@gmail.com>
2026-06-13 17:15:07 -0300

    Core classes

---
+++ src/model/core.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+@dataclass
+class Asset:
+    """Class to represent a project's asset"""
+    unique_name: str
+    path: Path
+
+@dataclass 
+class Entity:
+    """A class to represent a type of a entity, such a certain enemy, a rock, etc"""
+    unique_name: str
+    sprite: Asset
+    width: int
+    height: int
+    script_path: Path
+    variables: dict[str, Any]
+    hooks: dict[str, str] #[value_passed_to_on_message, real_function_within_script]
+
+@dataclass
+class InstancedEntity:
+    """A wrapper around Entity that represent a instanced Entity within a scene"""
+    entity: Entity
+    id: str
+    x: int
+    y: int
+
+@dataclass
+class Scene:
+    unique_name: str
+    background: Asset
+    entities: list[InstancedEntity]
+
+@dataclass
+class WindowSpecs:
+    title: str
+    height: int
+    width: int
+    target_fps: int
+
+
+@dataclass
+class ProjectSpecs:
+    name: str
+    engine_version: str
+    window_specs: WindowSpecs
+    initial_scene: Scene
