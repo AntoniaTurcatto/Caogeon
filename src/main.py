c56ada903c91ac462982f7fd4516997f9b550fdb
antoniaturcatto <antoniaturcatto29@gmail.com>
2026-06-29 00:18:58 -0300

    o mesmo ponto de entrada para player e editor

--- src/main.py
+++ src/main.py
@@ -7,21 +7,34 @@ from core.project_manager import ProjectManager
 from editor.dialogs.manager import DialogManager
 from editor.main_window import MainWindow
 
-def init_editor(app: QApplication, project_mgr: ProjectManager, dialogs_mgr: DialogManager):
-
-  main_window: MainWindow = MainWindow(project_mgr, dialogs_mgr)
-  main_window.showMaximized()
-  sys.exit(app.exec())
-
-def main():
+def init_editor(project_mgr: ProjectManager):
   app: QApplication = QApplication(sys.argv)
-  project_mgr = ProjectManager()
   dialogs_mgr = DialogManager(
     asset_manager=project_mgr.asset_manager,
     scene_manager=project_mgr.scene_manager,
     entity_manager=project_mgr.entity_manager
   )
-  init_editor(app, project_mgr, dialogs_mgr)
+  main_window: MainWindow = MainWindow(project_mgr, dialogs_mgr)
+  main_window.showMaximized()
+  sys.exit(app.exec())
+
+def main():
+  project_mgr = ProjectManager()
+  if len(sys.argv) == 1:
+    init_editor(project_mgr)
+    return
+
+  mode = sys.argv[1].lower()
+  if mode == "p":
+    init_player(project_mgr)
+    return
+
+  print(f"Unknown mode: {mode}")
+  print("Usage:")
+  print("  python main.py")
+  print("  python main.py p")
+  sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
